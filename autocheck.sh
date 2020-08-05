2ff84f8372f88f75b7f113c82b34d95ebedc3c81
Licolnlee <me@licolnlee.icu>
2020-08-05 23:05:18 +0800

    Update autocheck.sh

--- autocheck.sh
+++ autocheck.sh
@@ -23,9 +23,6 @@ https://raw.githubusercontent.com/ilpl/ad-hosts/master/hosts
 https://gitee.com/lhzgl6587/hosts/raw/master/hosts
 https://raw.githubusercontent.com/Amazefcc233/no-ads-hosts/master/hosts
 https://raw.githubusercontent.com/easonjim/blackhosts/master/hosts
-https://raw.githubusercontent.com/mitchellkrogza/Ultimate.Hosts.Blacklist/master/hosts/hosts0
-https://raw.githubusercontent.com/mitchellkrogza/Ultimate.Hosts.Blacklist/master/hosts/hosts1
-https://raw.githubusercontent.com/notracking/hosts-blocklists/master/hostnames.txt
 https://raw.githubusercontent.com/BlackJack8/iOSAdblockList/master/iPv4Hosts.txt
 EOF
 
