26960de89c922262a95a4e098d6c73aeba7148ae
Licolnlee <me@licolnlee.icu>
2022-04-09 22:31:45 +0800

    Update autocheck.sh

--- autocheck.sh
+++ autocheck.sh
@@ -25,6 +25,9 @@ https://raw.githubusercontent.com/Amazefcc233/no-ads-hosts/master/hosts
 https://raw.githubusercontent.com/easonjim/blackhosts/master/hosts
 https://raw.githubusercontent.com/Licolnlee/AdBlockList/master/AdBlockLiteTest-dnsmasq
 https://raw.githubusercontent.com/BlackJack8/iOSAdblockList/master/iPv4Hosts.txt
+https://hblock.molinero.dev/hosts_adblock.txt
+https://files.extstars.com/hosts/hosts
+https://abp.oisd.nl/
 EOF
 
 # 保留必要host
